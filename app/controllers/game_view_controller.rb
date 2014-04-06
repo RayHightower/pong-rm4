bbb9fd5b963fb849462a886942bd05bcf8efdae5
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-05 21:57:31 -0500

    experimenting with frame size

--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -7,7 +7,6 @@ class GameViewController < UIViewController
     @paddle_size = [20, 100]
 
     # self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    # self.view = UIView.alloc.initWithFrame([[0,0], [1136, 640]])
     self.view = UIView.alloc.initWithFrame([[0,0], [568, 320]])
 
     # RGB 40, 99, 34 = UIColor.tennisCourtGreenColor :-)
