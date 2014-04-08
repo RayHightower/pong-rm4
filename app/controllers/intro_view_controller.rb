0854e4d303cb4ee4fa297bf439feed993b747315
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-08 19:39:38 -0400

    cleanup

--- app/controllers/intro_view_controller.rb
+++ app/controllers/intro_view_controller.rb
@@ -28,7 +28,6 @@ class IntroViewController < UIViewController
     @startbutton.sizeToFit
     upper_left_button = [ (self.view.frame.size.height - button_width) / 2, self.view.frame.size.width / 2 ]
     @startbutton.frame = [ upper_left_button, [button_width, 30]]
-    # @startbutton.center = [self.view.frame.size.height / 2, self.view.frame.size.width]
     @startbutton.addTarget(self, action:"start_game", forControlEvents:UIControlEventTouchUpInside)
     self.view.addSubview(@startbutton)
   end
