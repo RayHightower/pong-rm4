ea2e59a8d90ea608d67c97d24c51079a41083338
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-11 20:42:35 -0400

    cleanup

--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -42,8 +42,7 @@ class GameViewController < UIViewController
 
   def create_right_paddle
     @right_paddle_view = PaddleView.alloc.initWithFrame [[(@height-70),50], @paddle_size]
-    # @right_paddle_view.center = [self.view.size.height-20, self.view.size.width/2] # if x>160+/-, the  right paddle  will not respond to touch events.
-    @right_paddle_view.center = [ self.view.size.width - 20, self.view.size.height/2] # if x>160+/-, the  right paddle  will not respond to touch events.
+    @right_paddle_view.center = [ self.view.size.width - 20, self.view.size.height/2]
     @right_paddle_view.backgroundColor = UIColor.whiteColor
     self.view.addSubview(@right_paddle_view)
   end
